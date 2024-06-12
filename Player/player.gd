00cf27832ff8d79d34bb66192eb74e053d302d84
Avram Eisner <avrame@gmail.com>
2024-06-11 17:35:20 -0700

    Added flapping animation

--- Player/player.gd
+++ Player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const FLAP_VELOCITY = -300.0
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var gravity_on = false
@@ -9,7 +11,7 @@ var dead = false
 signal died
 
 func _ready():
-	pass
+	_animated_sprite.play()
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -30,6 +32,9 @@ func _on_root_flap():
 
 func _on_ground_body_entered(body):
 	velocity.y = 0
+	position.y = 390
+	_animated_sprite.stop()
+	_animated_sprite.frame = 1
 	dead = true
 	died.emit()
 	$Crash.play()
